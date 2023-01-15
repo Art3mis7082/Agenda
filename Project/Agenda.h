c76dadd328d439e85265fcd27b5302dcc5b3165f
Beatriz Almaraz <beatriz.almagarc@outlook.com>
2023-01-14 18:40:13 -0600

    Modificaciones de Agenda.h, agregué documentación doxyfile a la SLL y hice unos cambios en en el menu dentro de sus funciones

--- Project/Agenda.h
+++ Project/Agenda.h
@@ -1,5 +1,5 @@
-#ifndef AGENDA
-#define AGENDA
+#ifndef AGENDA_INC
+#define AGENDA_INC
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -55,23 +55,83 @@ typedef struct SLL
 	size_t len;
 } SLL;
 
+/**
+ * @brief Crea una nueva lista SLL
+ * @return Referencia a la nueva lista
+ */
 SLL*   SLL_New();
+/**
+ * @brief Destruye una lista SLL
+ * @param this Referencia a un objeto SLL
+ * @post La lista se destruyó y no debiera ser utilizado de nuevo
+ */
 void   SLL_Delete( SLL** this );
 
+/**
+ * @brief Inserta un elemento en el frente de la lista.
+ * @param this Referencia a un objeto SLL.
+ * @param data El elemento a insertar.
+ * @pre La lista debe existir.
+ */
 void   SLL_Push_back( SLL* this, Tipo data );
+/**
+ * @brief Inserta un elemento a la derecha del cursor.
+ * @param this Referencia a un objeto SLL.
+ * @param data El elemento a insertar.
+ * @pre La lista debe existir.
+ * @post El cursor se mueve a la derecha, es decir, al nuevo nodo.
+ */
 void   SLL_Insert( SLL* this, Tipo data );
+
+/**
+ * @brief Devuelve una copia del elemento indicado por el cursor. El
+ * comportamiento es indefinido si el cursor no apunta a una posición
+ * válida.
+ * @param this Referencia a un objeto SLL.
+ * @return Una copia del elemento indicado por el cursor
+ */
+int    SLL_Get( SLL* this );
 
 void   SLL_Pop_front( SLL* this );
 
-int    SLL_Get( SLL* this );
+/**
+ * @brief Devuelve el número de elementos actualmente en la lista.
+ * @param this Referencia a un objeto SLL.
+ * @return El número de elementos actualmente en la lista.
+ */
 size_t SLL_Len( SLL* this );
 
+/**
+ * @brief Indica si la lista está vacía
+ * @param this Referencia a un objeto SLL
+ * @return true si la lista está vacía; false en cualquier otro caso
+ */
 bool   SLL_Is_empty( SLL* this );
+/**
+ * @brief Vacía la lista sin destruirla.
+ * @param this Referencia a un objeto SLL.
+ * @post La lista queda vacía.
+ */
 void   SLL_Make_empty( SLL* this );
 
 void   SLL_Cursor_front( SLL* this );
 void   SLL_Cursor_back( SLL* this );
+/**
+ * @brief Mueve al cursor al siguiente elemento a la derecha.
+ * @param this Referencia a un objeto SLL.
+ * @return true si el cursor se pudo mover a la derecha; 
+ *         false en caso contrario.
+ * @post El cursor NO se mueve si a la entrada apuntaba a NULL.
+ */
 void   SLL_Cursor_next( SLL* this );
+
+/**
+ * @brief Mueve al cursor al siguiente elemento a la derecha.
+ * @param this Referencia a un objeto SLL.
+ * @return true si el cursor se pudo mover a la derecha; 
+ *         false en caso contrario.
+ * @post El cursor NO se mueve si a la entrada apuntaba a NULL.
+ */
 bool   SLL_Find( SLL* this, int key );
 
 //----------------------------------------------------------------------
