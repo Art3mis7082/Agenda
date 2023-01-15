8589c9e1c06d1522896c2b0f7a1eb5c8887e1448
Beatriz Almaraz <beatriz.almagarc@outlook.com>
2023-01-15 09:56:06 -0600

    Documentación doxygen.

--- Project/Agenda.h
+++ Project/Agenda.h
@@ -24,19 +24,42 @@ typedef struct Agenda
 } Agenda; 
 
 
+/// @brief Crea un objeto agenda.
+/// @return Dirección asignada para la nueva agenda.
 Agenda* Agenda_new();
-
+/// @brief Borra la agenda (SLL y DLL de los TADS)
+/// @param this Objeto agenda
 void   Agenda_Delete( Agenda** this );
 
+/// @brief Añadir evento a la agenda
+/// @param this Objeto agenda
+/// @param data parámetros del objeto Evento
 void   Agenda_add_date( Agenda* this, Evento data );
+/// @brief Añadir contacto a la agenda
+/// @param this Objeto agenda 
+/// @param data nombre y número telefónico del contacto
 void   Agenda_add_contact(Agenda* this, Contacto data );
 
-
+/// @brief Verifica que las listas no estén vacías
+/// @param this Objeto agenda
+/// @return Si las listas de contactos y eventos están vacías
 bool   Agenda_Is_empty( Agenda* this );
+/// @brief Vacía las listas de Agenda (TAD contactos y eventos)
+/// @param  this Objeto agenda
 void   Agenda_Make_empty( Agenda* this );
 
+/// @brief Busca un evento por medio de un nombre
+/// @param this Objeto agenda
+/// @param key nombre del evento
+/// @return El evento que se busca
 bool   Agenda_Find_Event( Agenda* this, Evento key );
+/// @brief Busca un contacto dentro de la lista de contactos por número
+/// @param this Objeto agenda 
+/// @param key número de la lista de contactos
+/// @return El contacto según el número dado
 bool   Agenda_Find_contacto( Agenda* this, char key[MAX_CONTACTS]);
+/// @brief Imprime la agenda del mes
+/// @param this Objeto agenda
 void   Print_Agenda( Agenda* this );
 
 #endif   /* ----- #ifndef DUMMYCLASS_INC  ----- */
